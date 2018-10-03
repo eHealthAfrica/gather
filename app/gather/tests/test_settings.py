a0f6277d61348cff2c351e5aeff6cef244ea573f
obdulia <obdulia.losantos@gmail.com>
2018-10-03 12:50:03 +0200

    chore: introducing Aether CouchDB Sync module

--- app/gather/tests/test_settings.py
+++ app/gather/tests/test_settings.py
@@ -34,7 +34,7 @@ class SettingsTest(TestCase):
         self.assertEqual(settings.ROOT_URLCONF, 'gather.urls')
         self.assertEqual(settings.APP_NAME, 'Gather')
         self.assertEqual(settings.INSTANCE_NAME, 'Gather 3')
-        self.assertEqual(settings.AETHER_MODULES, ['kernel', 'odk'])
+        self.assertEqual(settings.AETHER_MODULES, ['kernel', 'odk', 'couchdb-sync'])
 
         self.assertIn('kernel', settings.AETHER_APPS)
         self.assertEqual(settings.AETHER_APPS['kernel']['url'], 'http://kernel-test:9000')
