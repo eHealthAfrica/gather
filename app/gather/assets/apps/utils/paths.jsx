1ba41262fcdeee0f6106ebf5b1a4564058feeab2
obdulia <obdulia.losantos@gmail.com>
2018-10-17 09:50:50 +0200

    fix: sync endpoint is mobile-users

--- app/gather/assets/apps/utils/paths.jsx
+++ app/gather/assets/apps/utils/paths.jsx
@@ -51,13 +51,13 @@ export const getSurveyorsAPIPath = ({ id, ...params }) => {
 }
 
 /**
- * Returns the API url to get the Sync Users data
+ * Returns the API url to get the Sync/Mobile Users data
  *
  * @param {number}  id          - sync user id
  * @param {object}  params      - query string parameters
  */
 export const getSyncUsersAPIPath = ({ id, ...params }) => {
-  return buildAPIPath(COUCHDB_SYNC_APP, 'sync-users', id, params)
+  return buildAPIPath(COUCHDB_SYNC_APP, 'mobile-users', id, params)
 }
 
 /**
