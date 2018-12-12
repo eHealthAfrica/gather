a869a1865e1b4a5c3ca569f6539dde86af2c266b
obdulia <obdulia.losantos@gmail.com>
2018-12-12 18:25:19 +0100

    feat(export): implement new options

--- app/gather/assets/apps/survey/entity/EntitiesDownload.jsx
+++ app/gather/assets/apps/survey/entity/EntitiesDownload.jsx
@@ -24,7 +24,7 @@ import { FormattedMessage } from 'react-intl'
 import { Portal } from '../../components'
 
 import { range } from '../../utils'
-import { MAX_PAGE_SIZE } from '../../utils/constants'
+import { MAX_PAGE_SIZE, EXPORT_CSV_FORMAT, EXPORT_EXCEL_FORMAT } from '../../utils/constants'
 import { getEntitiesAPIPath } from '../../utils/paths'
 import { postData } from '../../utils/request'
 
@@ -33,41 +33,23 @@ export default class EntitiesDownload extends Component {
     super(props)
 
     this.state = {
-      preparing: false
+      preparing: false,
+      // default export options
+      dataFormat: 'split',
+      fileFormat: EXPORT_CSV_FORMAT,
+      headerContent: 'labels',
+      headerSeparator: '/',
+      headerFull: true,
+      csvEscape: '\\',
+      csvSeparator: ',',
+      csvQuote: '"'
     }
   }
 
   render () {
-    const { survey, total } = this.props
-    const { EXPORT_FORMAT, EXPORT_MAX_ROWS_SIZE } = this.props.settings
-
+    const { total, filename } = this.props
+    const { EXPORT_MAX_ROWS_SIZE } = this.props.settings
     const pageSize = Math.min(EXPORT_MAX_ROWS_SIZE || MAX_PAGE_SIZE, MAX_PAGE_SIZE)
-    const params = {
-      project: survey.id,
-      format: '',
-      action: EXPORT_FORMAT,
-      pageSize
-    }
-
-    const download = (page, filename) => {
-      this.setState({ preparing: true, error: null })
-
-      return postData(
-        getEntitiesAPIPath({ ...params, page }),
-        {
-          paths: this.props.paths,
-          labels: this.props.labels,
-          filename
-        },
-        { download: true }
-      )
-        .then(() => {
-          this.setState({ preparing: false, error: null })
-        })
-        .catch(error => {
-          this.setState({ preparing: false, error })
-        })
-    }
 
     const icon = this.state.preparing
       ? <i className='fa fa-spinner fa-pulse mr-2' />
@@ -77,11 +59,15 @@ export default class EntitiesDownload extends Component {
       return (
         <React.Fragment>
           { this.renderError() }
+          { this.renderOptions() }
           <button
             type='button'
             className='tab'
             disabled={this.state.preparing}
-            onClick={() => { download(1, this.props.filename) }}
+            onClick={() => {
+              console.log(1, filename)
+              this.setState({ page: 1, filename })
+            }}
           >
             { icon }
             <FormattedMessage
@@ -95,13 +81,14 @@ export default class EntitiesDownload extends Component {
     const dropdown = 'downloadLinkChoices'
     const pages = range(1, Math.ceil(total / pageSize) + 1)
       .map(index => ({
-        currentPage: index,
-        filename: `${this.props.filename}-${index}`
+        page: index,
+        filename: `${filename}-${index}`
       }))
 
     return (
       <div className='dropdown'>
         { this.renderError() }
+        { this.renderOptions() }
         <button
           type='button'
           className='tab'
@@ -123,10 +110,10 @@ export default class EntitiesDownload extends Component {
             {
               pages.map(pageOptions => (
                 <button
-                  key={pageOptions.currentPage}
+                  key={pageOptions.page}
                   type='button'
                   className='dropdown-item'
-                  onClick={() => { download(pageOptions.currentPage, pageOptions.filename) }}
+                  onClick={() => { this.setState({ ...pageOptions }) }}
                 >
                   { pageOptions.filename }
                 </button>
@@ -136,6 +123,317 @@ export default class EntitiesDownload extends Component {
         </div>
       </div>
     )
+  }
+
+  renderOptions () {
+    const { page, filename } = this.state
+
+    if (!page) {
+      return ''
+    }
+
+    const { survey } = this.props
+    const { EXPORT_MAX_ROWS_SIZE } = this.props.settings
+
+    const pageSize = Math.min(EXPORT_MAX_ROWS_SIZE || MAX_PAGE_SIZE, MAX_PAGE_SIZE)
+    const params = {
+      project: survey.id,
+      format: '',
+      action: this.state.fileFormat,
+      pageSize
+    }
+
+    const download = () => {
+      this.setState({ preparing: true, page: 0, error: null })
+
+      return postData(
+        getEntitiesAPIPath({ ...params, page }),
+        {
+          paths: this.props.paths,
+          labels: this.props.labels,
+          data_format: this.state.dataFormat,
+          header_content: this.state.headerContent,
+          header_separator: this.state.headerSeparator,
+          header_shorten: this.state.headerFull ? 'no' : 'yes',
+          csv_escape: this.state.csvEscape,
+          csv_separator: this.state.csvSeparator === 'TAB' ? '\t' : this.state.csvSeparator.charAt(0),
+          csv_quote: this.state.csvQuote,
+          filename
+        },
+        { download: true }
+      )
+        .then(() => {
+          this.setState({ preparing: false, error: null })
+        })
+        .catch(error => {
+          this.setState({ preparing: false, error })
+        })
+    }
+
+    const onInputChange = (event) => {
+      event.preventDefault()
+      this.setState({ [event.target.name]: event.target.value })
+    }
+
+    const DATA_FORMATS = [
+      {
+        id: 'split',
+        label: (
+          <FormattedMessage
+            id='entities.download.data.format.split'
+            defaultMessage='Normalised into multiple files (better for importing into a relational database)' />
+        )
+      },
+      {
+        id: 'flatten',
+        label: (
+          <FormattedMessage
+            id='entities.download.data.format.flatten'
+            defaultMessage='Flattened into a single file (better for manual analysis and some other tools)' />
+        )
+      }
+    ]
+
+    const HEADERS = [
+      {
+        id: 'labels',
+        label: (
+          <FormattedMessage
+            id='entities.download.headers.labels'
+            defaultMessage='Labels' />
+        )
+      },
+      {
+        id: 'paths',
+        label: (
+          <FormattedMessage
+            id='entities.download.headers.paths'
+            defaultMessage='Names' />
+        )
+      },
+      {
+        id: 'both',
+        label: (
+          <FormattedMessage
+            id='entities.download.headers.both'
+            defaultMessage='Both' />
+        )
+      }
+    ]
+
+    const FILE_FORMATS = [
+      {
+        id: EXPORT_EXCEL_FORMAT,
+        label: EXPORT_EXCEL_FORMAT.toUpperCase()
+      },
+      {
+        id: EXPORT_CSV_FORMAT,
+        label: EXPORT_CSV_FORMAT.toUpperCase(),
+        // in case of checked, render the CSV options
+        renderChecked: () => (
+          <div className='ml-5 form-inline'>
+            <div className='form-group mr-5'>
+              <label className='form-control-label title mr-2'>
+                <FormattedMessage
+                  id='entities.download.csv.separator'
+                  defaultMessage='Delimiter' />
+              </label>
+              <input
+                name='csvSeparator'
+                type='text'
+                className='form-control'
+                size={3}
+                maxLength={3}
+                value={this.state.csvSeparator || ''}
+                onChange={onInputChange}
+              />
+            </div>
+            <div className='form-group mr-5'>
+              <label className='form-control-label title mr-2'>
+                <FormattedMessage
+                  id='entities.download.csv.quote'
+                  defaultMessage='Quote' />
+              </label>
+              <input
+                name='csvQuote'
+                type='text'
+                className='form-control'
+                size={3}
+                maxLength={1}
+                value={this.state.csvQuote || ''}
+                onChange={onInputChange}
+              />
+            </div>
+            <div className='form-group mr-5'>
+              <label className='form-control-label title mr-2'>
+                <FormattedMessage
+                  id='entities.download.csv.escape'
+                  defaultMessage='Escape' />
+              </label>
+              <input
+                name='csvEscape'
+                type='text'
+                className='form-control'
+                size={3}
+                maxLength={1}
+                value={this.state.csvEscape || ''}
+                onChange={onInputChange}
+              />
+            </div>
+          </div>
+        )
+      }
+    ]
+
+    return (
+      <Portal>
+        <div className='modal show'>
+          <div className='modal-dialog modal-dialog-centered modal-lg'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title'>
+                  <FormattedMessage
+                    id='entities.download.options.title'
+                    defaultMessage='Download data' />
+                </h5>
+                <button
+                  data-qa='confirm-button-close'
+                  type='button'
+                  className='close'
+                  onClick={() => { this.setState({ page: 0, filename: null }) }}>
+                  &times;
+                </button>
+              </div>
+
+              <div className='modal-body'>
+                <div className='m-3'>
+                  <h6 className='border-bottom p-2 mb-3'>
+                    <FormattedMessage
+                      id='entities.download.data.format.title'
+                      defaultMessage='Data format' />
+                  </h6>
+                  { this.renderChoices(DATA_FORMATS, 'dataFormat') }
+                </div>
+
+                <div className='m-3'>
+                  <h6 className='border-bottom p-2 mb-3'>
+                    <FormattedMessage
+                      id='entities.download.headers.title'
+                      defaultMessage='Headers' />
+                  </h6>
+
+                  { this.renderChoices(HEADERS, 'headerContent', 'd-inline mr-5') }
+
+                  <div className='ml-5 form-inline'>
+                    <div
+                      className='form-group mr-5'
+                      onClick={() => { this.setState({ headerFull: !this.state.headerFull }) }}>
+                      <input
+                        name='headerFull'
+                        type='checkbox'
+                        className='form-control mr-2'
+                        checked={this.state.headerFull}
+                        readOnly
+                      />
+                      <label className='form-control-label title'>
+                        <FormattedMessage
+                          id='entities.download.headers.full'
+                          defaultMessage='Show full path in headers' />
+                      </label>
+                    </div>
+
+                    { this.state.headerFull &&
+                      <div className='form-group mr-5'>
+                        <label className='form-control-label title mr-2'>
+                          <FormattedMessage
+                            id='entities.download.headers.separator'
+                            defaultMessage='Delimiter' />
+                        </label>
+                        <input
+                          name='headerSeparator'
+                          type='text'
+                          className='form-control'
+                          size={3}
+                          maxLength={1}
+                          value={this.state.headerSeparator || ''}
+                          onChange={onInputChange}
+                        />
+                      </div>
+                    }
+                  </div>
+                </div>
+
+                <div className='m-3'>
+                  <h6 className='border-bottom p-2 mb-3'>
+                    <FormattedMessage
+                      id='entities.download.file.format.title'
+                      defaultMessage='File format' />
+                  </h6>
+
+                  { this.renderChoices(FILE_FORMATS, 'fileFormat') }
+                </div>
+              </div>
+
+              <div className='modal-footer'>
+                <div className='w-100 actions'>
+                  <div>
+                    <button
+                      type='button'
+                      className='btn btn-cancel btn-block'
+                      onClick={() => { this.setState({ page: 0, filename: null }) }}>
+                      <FormattedMessage
+                        id='entities.download.cancel'
+                        defaultMessage='Cancel' />
+                    </button>
+                  </div>
+
+                  <div>
+                    <button
+                      type='button'
+                      className='btn btn-primary btn-block'
+                      onClick={() => { download() }}>
+                      <FormattedMessage
+                        id='entities.download.prepare'
+                        defaultMessage='Prepare' />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Portal>
+    )
+  }
+
+  renderChoices (list, name, className) {
+    return list.map(option => (
+      <div
+        key={option.id}
+        className={`form-group pl-2 ${className || ''}`}>
+        <div
+          onClick={() => { this.setState({ [name]: option.id }) }}>
+          <input
+            name={name}
+            value={option.id}
+            type='radio'
+            checked={this.state[name] === option.id}
+            readOnly
+            className='mr-2'
+          />
+          <label>
+            { option.label }
+          </label>
+        </div>
+
+        { this.state[name] === option.id &&
+          option.renderChecked &&
+          <div>
+            { option.renderChecked() }
+          </div>
+        }
+      </div>
+    ))
   }
 
   renderError () {
