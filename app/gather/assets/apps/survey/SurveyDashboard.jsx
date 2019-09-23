f2f789b531ec0a0df172b5b018fa538317b8ed8a
Umar Muhammad <umarmuhammad3516@gmail.com>
2019-09-23 20:00:05 +0100

    config screen implementation

--- app/gather/assets/apps/survey/SurveyDashboard.jsx
+++ app/gather/assets/apps/survey/SurveyDashboard.jsx
@@ -4,7 +4,7 @@
  * See the NOTICE file distributed with this work for additional information
  * regarding copyright ownership.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
@@ -12,16 +12,48 @@
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 import { FormattedMessage } from 'react-intl'
+import SurveyConfig from './mask/surveyConfig'
 
-const SurveyDashboard = () => (
+const SurveyDashboard = ({ columns, labels, entities_count, dashboardConfig, saveDashboardConfig }) => {
+  const [showConf, setShowConf] = useState(false)
+  return (
+    <div>
+      {
+        entities_count > 0 && (
+          <p className='alert alert-info'>
+            <a href='#!' role='button' onClick={() => setShowConf(!showConf)}>
+              <FormattedMessage
+                id='alert.loading'
+                defaultMessage={showConf ? 'Close' : 'Dashboard Configuration'}
+              />
+            </a>
+          </p>
+        )
+      }
+      {
+        showConf &&
+        <SurveyConfig
+          setShowConf={setShowConf}
+          dashboardConfig={dashboardConfig}
+          saveDashboardConfig={saveDashboardConfig}
+          columns={columns}
+          labels={labels}
+        />
+      }
+      {entities_count > 0 && !columns.length && !showConf && renderNoDashboard(setShowConf)}
+    </div>
+  )
+}
+
+const renderNoDashboard = setShowConf => (
   <div className='survey-content no-dashboard'>
     <h4 className='headline'>
       <FormattedMessage
@@ -31,8 +63,19 @@ const SurveyDashboard = () => (
     </h4>
     <FormattedMessage
       id='survey.no.dashboard.help-2'
-      defaultMessage='When you activate the dashboard, data will be sent to Elastic Search.'
+      defaultMessage='You can configure data to be sent to Elastic Search and activate a Kibana Dashboard.'
     />
+    <br />
+    <button
+      type='button'
+      className='btn btn-primary btn-secondary'
+      onClick={() => setShowConf(true)}
+    >
+      <FormattedMessage
+        id='survey.no.dashboard.button'
+        defaultMessage='Configure dashboard now'
+      />
+    </button>
   </div>
 )
 
---
+++ app/gather/assets/apps/survey/mask/surveyConfig.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react'
+import { defineMessages, injectIntl, FormattedMessage } from 'react-intl'
+import { getLabelTree } from '../../utils/types'
+
+const prefix = 'survey.dashboard.config'
+const visualizations = ['No Visualization', 'Bar Chart', 'Line Chart', 'Pie Chart']
+
+const MESSAGES = {
+  button: {
+    id: `${prefix}.button`,
+    defaultMessage: 'Activate dashboard'
+  },
+  visualizations: visualizations.reduce(
+    (acc, curr) => ({
+      ...acc,
+      [curr]: { id: `${prefix}.${curr.replace(' ', '_')}`, defaultMessage: curr }
+    }),
+    {}
+  )
+}
+
+export default ({ dashboardConfig, saveDashboardConfig, setShowConf, columns, labels }) => {
+  const [newDashboardConfig, setNewDashboardConfig] = useState({})
+
+  useEffect(() => {
+    setNewDashboardConfig(dashboardConfig || columns.reduce(
+      (acc, column) => ({ ...acc, [getLabelTree(column, labels)]: { elastic: false, dashboard: null } }),
+      {}
+    ))
+  }, [])
+
+  const handleElastic = (key, { target: { checked } }) =>
+    setNewDashboardConfig({
+      ...newDashboardConfig,
+      [key]: { elastic: checked, dashboard: checked ? visualizations[0] : null }
+    })
+
+  const handleDashboard = (key, value) =>
+    setNewDashboardConfig({
+      ...newDashboardConfig,
+      [key]: { ...newDashboardConfig[key], dashboard: value }
+    })
+
+  return (
+    <div className='config-screen'>
+      <div className='content'>
+        <div className='row'>
+          <div className='col-8' />
+          <div className='col-2'><h5>Elastic Search</h5></div>
+          <div className='col-2'><h5>Dashboard</h5></div>
+        </div>
+        <ul>
+          {
+            Object.keys(newDashboardConfig).map((key, index) => {
+              const item = newDashboardConfig[key];
+              const itemName = `${index + 1}. ${key}`
+              return (
+                <li key={itemName} className='item-title'>
+                  <div className='row row-item'>
+                    <div className='col-8 label-col'>{itemName}</div>
+                    <div className='col-2 form-check'>
+                      <input
+                        className='form-check-input'
+                        type='checkbox'
+                        checked={item.elastic}
+                        onChange={event => handleElastic(key, event)}
+                      />
+                    </div>
+                    {
+                      item.elastic && (
+                        <div className='col-2'>
+                          <div className='dropdown'>
+                            <div className='dropdown-toggle' href='#!' data-toggle='dropdown'>
+                              <FormattedMessage
+                                id={
+                                  item.dashboard
+                                  ? MESSAGES.visualizations[item.dashboard].id
+                                  : `${prefix}.${visualizations[0].replace(' ', '_')}`
+                                }
+                                defaultMessage={item.dashboard || visualizations[0]}
+                              />
+                              <span className='caret' />
+                            </div>
+                            <div className='dropdown-menu'>
+                              {visualizations.map((elem, index) => (
+                                <a
+                                  key={`${elem}${index}`}
+                                  className='dropdown-item'
+                                  href='#!'
+                                  onClick={event => handleDashboard(key, elem)}
+                                >
+                                  <FormattedMessage
+                                    id={MESSAGES.visualizations[elem].id}
+                                    defaultMessage={MESSAGES.visualizations[elem].defaultMessage}
+                                  />
+                                </a>
+                              ))}
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    }
+                  </div>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+      <div className='config-btn'>
+          <button
+            type='button'
+            className='btn btn-primary btn-secondary'
+            onClick={() => {
+              setShowConf(false)
+              saveDashboardConfig(newDashboardConfig)
+            }}
+          >
+            <FormattedMessage
+              id={MESSAGES.button.id}
+              defaultMessage={MESSAGES.button.defaultMessage}
+            />
+          </button>
+        </div>
+    </div>
+  )
+}
