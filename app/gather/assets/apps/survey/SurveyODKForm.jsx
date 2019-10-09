af07c741b28605b71392b73f80b97d6641330015
Umar Muhammad <umarmuhammad3516@gmail.com>
2019-10-09 16:52:42 +0100

    Fix value incompatibility

--- app/gather/assets/apps/survey/SurveyODKForm.jsx
+++ app/gather/assets/apps/survey/SurveyODKForm.jsx
@@ -35,6 +35,7 @@ import {
 } from '../components'
 
 import { getMediaFileContentPath } from '../utils/paths'
+import { selectUnit } from '@formatjs/intl-utils';
 
 const MESSAGES = defineMessages({
   newForm: {
@@ -308,6 +309,16 @@ class XForm extends Component {
     const allErrors = []
     Object.keys(errors).forEach(key => { allErrors.push(errors[key]) })
 
+    const { value, unit } = selectUnit(new Date(xform.created_at));
+    const date = (xform.id
+      ? (
+        <small className='mr-4'>
+          (<FormattedRelativeTime value={value} unit={unit} />)
+        </small>
+      )
+      : ''
+    )
+
     const title = (
       <span title={xform.description} className='form-title'>
         <i className='fas fa-file mr-2' />
@@ -343,11 +354,6 @@ class XForm extends Component {
           />
         </small>
       )
-    )
-
-    const date = (xform.id
-      ? <small className='mr-4'>(<FormattedRelativeTime value={xform.created_at} />)</small>
-      : ''
     )
 
     return (
