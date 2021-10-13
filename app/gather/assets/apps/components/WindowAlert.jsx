701f6453f4cbc0a405d8d19374a0bb0546a02f49
obdulia-losantos <obdulia.losantos@gmail.com>
2021-10-13 15:45:51 +0200

    feat: WindowAlert component

---
+++ app/gather/assets/apps/components/WindowAlert.jsx
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2021 by eHealth Africa : http://www.eHealthAfrica.org
+ *
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+import Portal from './Portal'
+
+const WindowAlert = ({ title, message }) => {
+  const [visible, setVisible] = React.useState(true)
+
+  const onCancel = () => setVisible(false)
+
+  React.useEffect(() => {
+    setVisible(!!message)
+  }, [message])
+
+  if (!visible) return ''
+
+  return (
+    <Portal onEscape={onCancel} onEnter={onCancel}>
+      <div data-qa='window-alert' className='confirmation-container'>
+        <div className='modal show'>
+          <div className='modal-dialog modal-md'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                {title && <h5 className='modal-title'>{title}</h5>}
+                <button
+                  data-qa='window-alert-close'
+                  type='button'
+                  className='btn-close'
+                  onClick={onCancel}
+                />
+              </div>
+
+              <div data-qa='window-alert-message' className='modal-body'>
+                {message}
+              </div>
+
+              <div className='modal-footer'>
+                <button
+                  data-qa='window-alert-ok'
+                  type='button'
+                  className='btn btn-secondary'
+                  onClick={onCancel}
+                >
+                  <FormattedMessage
+                    id='window.alert.button.action.close'
+                    defaultMessage='OK'
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Portal>
+  )
+}
+
+export default WindowAlert
---
+++ app/gather/assets/apps/components/WindowAlert.spec.jsx
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2019 by eHealth Africa : http://www.eHealthAfrica.org
+ *
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/* global describe, it, expect */
+
+import React from 'react'
+import { mountWithIntl } from '../../tests/enzyme-helpers'
+
+import WindowAlert from './WindowAlert'
+
+describe('WindowAlert', () => {
+  it('should render nothing without a message', () => {
+    const component = mountWithIntl(<WindowAlert />)
+    expect(component.find('[data-qa="window-alert"]').exists()).toBeFalsy()
+  })
+
+  it('should render the message', () => {
+    const component = mountWithIntl(<WindowAlert message='message' />)
+    expect(component.find('[data-qa="window-alert"]').exists()).toBeTruthy()
+    expect(component.find('[data-qa="window-alert-message"]').exists()).toBeTruthy()
+  })
+})
